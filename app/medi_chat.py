47574cac3db08d7d33223a2a9206e0110e30e931
Prasanna Kotyal <66207940+prasannakotyal@users.noreply.github.com>
2024-02-17 14:55:04 +0530

    Update medi_chat.py

--- app/medi_chat.py
+++ app/medi_chat.py
@@ -8,9 +8,9 @@ load_dotenv()
 
 # Configure Streamlit page settings
 st.set_page_config(
-    page_title="Chat with Gemini-Pro!",
+    page_title="Chat with medichat",
     page_icon=":brain:",  # Favicon emoji
-    layout="centered",  # Page layout option
+    layout="wide",  # Page layout option
 )
 
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
@@ -19,10 +19,6 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 gen_ai.configure(api_key=GOOGLE_API_KEY)
 model = gen_ai.GenerativeModel('gemini-pro')
 
-# Initialize chat session in Streamlit if not already present
-if "chat_session" not in st.session_state:
-    st.session_state.chat_session = model.start_chat(history=[])
-
 # Function to translate roles between Gemini-Pro and Streamlit terminology
 def translate_role_for_streamlit(user_role):
     if user_role == "model":
@@ -30,36 +26,30 @@ def translate_role_for_streamlit(user_role):
     else:
         return user_role
 
-# Function to handle the MediChat app
-def medichat_app():
-    # Display the chatbot's title on the page
-    st.title("‍⚕️ MediChat - Your Medical Assistant")
-
-    # Display the chat history
-    for message in st.session_state.chat_session.history:
-        with st.chat_message(translate_role_for_streamlit(message.role)):
-            st.markdown(message.parts[0].text)
-
-    # Input field for user's message
-    user_prompt = st.chat_input("Ask MediChat...")
-    if user_prompt:
-        # Add user's message to chat and display it
-        st.session_state.chat_session.add_user_message(user_prompt)
-        st.chat_message("user").markdown(user_prompt)
+# Initialize chat session in Streamlit if not already present
+if "chat_session" not in st.session_state:
+    st.session_state.chat_session = model.start_chat(history=[])
 
 # Streamlit UI
-st.title("Welcome to Care Compass")
+st.title("MediChat")
 
-# Create tabs
-tabs = ["MediChat", "Calorie Tracker", "Nearby Places"] # Add "Diet Recommender"
-selected_tab = st.radio("Choose Option", tabs)
+# Display MediChat tab
+st.title("👩‍⚕️- MediChat(Your personal medical assistant)")
 
-# Display the selected tab
-if selected_tab == "MediChat":
-    medichat_app()
-elif selected_tab == "Calorie Tracker":
-    calorie_tracker_app()
-elif selected_tab == "Nearby Places":
-    nearby_places_app()
-# elif selected_tab == "Diet Recommender":  # Add this block for the Diet Recommender tab
-#     diet_recommender_app()
+# Display the chat history
+for message in st.session_state.chat_session.history:
+    with st.chat_message(translate_role_for_streamlit(message.role)):
+        st.markdown(message.parts[0].text)
+
+# Input field for user's message
+user_prompt = st.chat_input("Ask medichat...")
+if user_prompt:
+    # Add user's message to chat and display it
+    st.chat_message("user").markdown(user_prompt)
+
+    # Send user's message to Gemini-Pro and get the response
+    gemini_response = st.session_state.chat_session.send_message(user_prompt)
+
+    # Display Gemini-Pro's response
+    with st.chat_message("assistant"):
+        st.markdown(gemini_response.text)
