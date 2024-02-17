4fdb4ae91c97ccda020a8a306a4e5aaa1c155528
Prasanna Kotyal <66207940+prasannakotyal@users.noreply.github.com>
2024-02-17 14:13:40 +0530

    Update medi_chat.py

--- app/medi_chat.py
+++ app/medi_chat.py
@@ -1,6 +1,6 @@
+import os
 import streamlit as st
 from dotenv import load_dotenv
-import os
 import google.generativeai as gen_ai
 
 # Load environment variables
@@ -28,31 +28,26 @@ def translate_role_for_streamlit(user_role):
 
 # Initialize chat session in Streamlit if not already present
 if "chat_session" not in st.session_state:
-    st.session_state.chat_session = None  # Initialize with None
+    st.session_state.chat_session = ''
 
 def medichat_app():
     st.title("👩‍⚕️- MediChat (Your personal medical assistant)")
 
-    # Ensure proper initialization of st.session_state
-    if not hasattr(st.session_state, 'chat_session') or st.session_state.chat_session is None:
-        st.session_state.chat_session = model.start_chat(history=[])
-
     # Display the chat history
-    for message in st.session_state.chat_session.history:
-        with st.chat_message(translate_role_for_streamlit(message.role)):
-            st.markdown(message.parts[0].text)
+    if st.session_state.chat_session:
+        for message in st.session_state.chat_session:
+            with st.chat_message(translate_role_for_streamlit(message.role)):
+                st.markdown(message.parts[0].text)
 
     # Input field for user's message
     user_prompt = st.chat_input("Ask medichat...")
     if user_prompt:
         # Add user's message to chat and display it
-        st.session_state.chat_session.send_message(user_prompt)
         st.chat_message("user").markdown(user_prompt)
 
         # Send user's message to Gemini-Pro and get the response
-        gemini_response = st.session_state.chat_session.get_response()
-        st.session_state.chat_session.add_message(role="assistant", content=gemini_response.text)
+        gemini_response = model.start_chat(history=[user_prompt])
 
         # Display Gemini-Pro's response
         with st.chat_message("assistant"):
-            st.markdown(gemini_response.text)
+            st.markdown(gemini_response.parts[0].text)
