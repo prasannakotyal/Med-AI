072f0a87bb56cd2ac443063b2cde7c5b57ca53a0
Prasanna Kotyal <66207940+prasannakotyal@users.noreply.github.com>
2024-08-07 00:06:04 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,13 +2,20 @@ import streamlit as st
 from medi_chat import medichat_app
 from calorie_tracker import calorie_tracker_app
 from nearby_places import nearby_places_app
-# from diet_recommender import diet_recommender_app  # Import the diet_recommender app
+from diet_recommender import diet_recommendation_app
+
+# Configure Streamlit page settings
+st.set_page_config(
+    page_title="Care Compass",
+    page_icon=":compass:",  # Favicon emoji
+    layout="centered",  # Full-width layout
+)
 
 # Streamlit UI
 st.title("Welcome to Care Compass")
 
 # Create tabs
-tabs = ["MediChat", "Calorie Tracker", "Nearby Places"] # Add "Diet Recommender"
+tabs = ["MediChat", "Calorie Tracker", "Nearby Places", "Diet Recommendation"]
 selected_tab = st.radio("Choose Option", tabs)
 
 # Display the selected tab
@@ -18,5 +25,5 @@ elif selected_tab == "Calorie Tracker":
     calorie_tracker_app()
 elif selected_tab == "Nearby Places":
     nearby_places_app()
-# elif selected_tab == "Diet Recommender":  # Add this block for the Diet Recommender tab
-#     diet_recommender_app()
+elif selected_tab == "Diet Recommendation":
+    diet_recommendation_app()
